e725271d0a6521712b5df7eda92ce1ad97d25d99
Tianyu Hu <122837880+u1297680@users.noreply.github.com>
2023-04-23 00:26:57 -0600

    Update viewparkinglot.php

--- viewparkinglot.php
+++ viewparkinglot.php
@@ -83,8 +83,7 @@
     <nav>
       <ul>
         <li><a href="HP.php"> Home Page </a></li>
-		<li><a href="addparkinglot.php">Add Lots</a></li>
-        <li><a href="updateparkinglot.php">Update Lots</a></li>
+	<li><a href="addparkinglot.php">Add Lots</a></li>
         <li><a href="deleteparkinglot.php">Delete Lots</a></li>
       </ul>
     </nav>
@@ -95,6 +94,8 @@
         <thead>
 <tr>
 	<th>LOT ID</th>
+	<th>Permit_Type_1</th>
+	<th>Permit_Type_2</th>
 	<th>Address</th>
 	<th>Capacity</th>
 
@@ -126,7 +127,9 @@ for($j=0; $j<$rows; $j++) {
 	<pre>
 		<tr>
 		
-		<td><a href='updateparkinglot.php?lot_id=$row[LOT_ID]' >$row[Permit_Type_1]</a>
+		<td><a href='updateparkinglot.php?lot_id=$row[LOT_ID]' >$row[LOT_ID]</a>
+		<td>$row[Permit_Type_1]
+		<td>$row[Permit_Type_2]
 		<td>$row[Address]
 		<td>$row[Capacity]
 
