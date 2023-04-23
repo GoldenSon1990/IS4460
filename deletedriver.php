e4b4390b0ce83c363ad3284851a078603c076649
GoldenSon1990 <123338478+GoldenSon1990@users.noreply.github.com>
2023-04-23 01:18:25 -0600

    Update deletedriver.php

--- deletedriver.php
+++ deletedriver.php
@@ -94,14 +94,15 @@
         <li><a href="HP.php">Home</a></li>
         <li><a href="viewdriver.php">View Drivers</a></li>
         <li><a href="adddriver.php">Add Drivers</a></li>
-		<li><a href="updatedriver.php">Update Drivers</a></li>
+	<li><a href="updatedriver.php">Update Drivers</a></li>
+	<li><a href="logout.php">Logout</a></li>
       </ul>
     </nav>
     <main>
 <?php
 
 require_once 'db.php';
-	    require_once 'checksession.php';
+require_once 'checksession.php';
 
 $conn = new mysqli($hn, $un, $pw, $db);
 if ($conn->connect_error) die($conn->connect_error);
